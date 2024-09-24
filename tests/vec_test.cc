0b2f63d328fc0a3419d0dffbd61a09ba2f534f98
Matheus Catarino <matheus-catarino@hotmail.com>
2024-09-24 16:34:03 -0300

    add test

---
+++ tests/vec_test.cc
@@ -0,0 +1,10 @@
+/// Reference:
+/// https://github.com/CppCon/CppCon2024/blob/main/Presentations/LLVMs_Realtime_Safety_Revolution.pdf
+
+#include <vector>
+
+int process(int) [[clang::nonblocking]] {
+  std::vector<int> v(16);
+  return v[16];
+}
+int main() { return process(0); }
